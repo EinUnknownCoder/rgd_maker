4a6d5216cb65c1cc867a50c4f77811e9a9d9a3d5
Ein Unknown <einunknowncoder@gmail.com>
2022-08-27 23:55:04 +0200

    V1.1

--- App.py
+++ App.py
@@ -1,13 +1,27 @@
-from cmath import exp
 from openpyxl import load_workbook
 from pytube import YouTube
 from pydub import AudioSegment
 import random
 from datetime import datetime
+import os
+
+# INPUT
+length = 0 #Length of the sound file
+participants = [] #List of Participants
+fair_distribution = False # Every member gets the same number of songs
+
+#SCRIPT
+print("Welcome to the RDG Generator!")
+
+print("\nLoading Excel...", end=' ')
 
 wb = load_workbook(filename="songlist.xlsx", read_only=True)
-
 sheet = wb["Songlist"]
+already_downloaded = os.listdir("raw")
+
+print("Done!")
+
+print("\nNow Loading songs!")
 
 playlist = []
 
@@ -20,25 +34,24 @@ for x in range(2, sheet.max_row + 1):
     start = sheet.cell(x, 5).value
     end = sheet.cell(x, 6).value
 
-    print(" ".join([url, title, artist, str(start), str(end)]))
-
-    yt = YouTube(url)
-
-    print(yt.title)
-
-    audio_stream = yt.streams.get_audio_only()
-
-    print(audio_stream)
-
     file_name = artist + " - " + title + ".mp4"
 
-    audio_stream.download("raw/", file_name)
+    if file_name not in already_downloaded:
+
+        print(f"\n{file_name} missing! Downloading... ", end=' ')
+
+        yt = YouTube(url)
+        audio_stream = yt.streams.get_audio_only()
+        audio_stream.download("raw/", file_name)
+
+        print("Done!")
+
+    else:
+        print(f"{file_name} is already downloaded! Skipping...")
 
     playlist.append(["raw/" + file_name, start, end])
 
 wb.close()
-
-print(playlist)
 
 random.shuffle(playlist)
 
@@ -48,9 +61,11 @@ starting_song = ending_song
 
 export = starting_song
 
+print("\nCreating Playlist with following order:")
+
 for x in playlist:
-    
-    print(x)
+
+    print(x[0].split("/")[1])
 
     start = x[1] - 10
     end = x[2] + 2
@@ -60,6 +75,12 @@ for x in playlist:
 
 export = export + ending_song
 
-print("Exporting...")
+print("\nExporting file...", end=' ')
 
-export.export("export/" + str(datetime.now()) + ".mp3", format="mp3")
+file_name = "".join([str(datetime.now().strftime("%Y-%m-%d %H_%M_%S"))]) + ".mp3"
+
+export.export("export/" + file_name , format="mp3")
+
+print("Done!")
+
+print(f"\nFile {file_name} is created!")
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # rgd_maker
+[Trello](https://trello.com/b/GVCmj2GP/rdg-maker)
+
+# Create MP4 for YouTube
+
+``ffmpeg -loop 1 -framerate 1 -i image.png -i audio.mp3 -map 0:v -map 1:a -r 10 -vf "scale='iw-mod(iw,2)':'ih-mod(ih,2)',format=yuv420p" -movflags +faststart -shortest -fflags +shortest -max_interleave_delta 100M output.mp4``
+
+[Documentation](https://stackoverflow.com/questions/64375367/python-convert-mp3-to-mp4-with-static-image)
